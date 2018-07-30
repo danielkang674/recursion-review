78ee147a47716c7ec6ed3ad358de6cc6fec4a1ef
Hack Reactor Student <student@hackreactor.com>
2018-07-30 12:23:30 -0500

    through code changing methods

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  let targetBody = document.body;
+  let bodyChildren = targetBody.children;
+  let targetClassArray = [];
+
+  let checkChildren = function(childArray){
+    for(var i = 0; i < childArray.length; i++){
+      if(childArray[i].className === className){
+        targetClassArray.push(childArray[i]);  
+        }
+      if(childArray[i].children()){
+        checkChildren(childArray[i]);      
+      }
+    } 
+  };
+  checkChildren(bodyChildren);
+
+  return targetClassArray;
 };
